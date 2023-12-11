a281ed0ba2cfefafc2a8a2cd3962bef7fcd108c4
kimgayeons <4402aaa@gmail.com>
2023-12-11 17:23:28 +0900

    session값 임의로 세팅

--- src/main/java/com/fastcampus/tasteofwork2/pay/controller/PayRestController.java
+++ src/main/java/com/fastcampus/tasteofwork2/pay/controller/PayRestController.java
@@ -1,11 +1,17 @@
 package com.fastcampus.tasteofwork2.pay.controller;
 
 import com.fastcampus.tasteofwork2.pay.vo.AjaxResultVO;
+import com.fastcampus.tasteofwork2.pay.vo.MemberVO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 @Slf4j
@@ -13,8 +19,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class PayRestController {
 
     @PostMapping("/payment")
-    public AjaxResultVO pay(@RequestParam("totalOrderPrice") Integer totalOrderPrice) {
+    public AjaxResultVO pay(HttpSession session, HttpServletRequest request, HttpServletResponse response, @RequestParam("totalOrderPrice") Integer totalOrderPrice) {
         log.info("totalOrderPrice:"+totalOrderPrice);
+
+        /*임의로 session에 값을 저장*/
+        MemberVO memberVO = new MemberVO();
+        memberVO.setMemberId("kimgy");
+        memberVO.setName("김가연");
+        memberVO.setTel("01012341234");
+        memberVO.setEmail("4402aaa@gmail.com");
+
+        session.setAttribute("userLoginInfo", memberVO);
+
+        MemberVO member = (MemberVO) session.getAttribute("userLoginInfo");
+        log.info("member:"+member);
+        String memberId = memberVO.getMemberId();
+        log.info("member:"+member.getMemberId());
+
+        response.setHeader("SET-COOKIE", "JSESSIONID=" + memberId + "; Path=/; Secure; SameSite=None");
+
+
         return null;
     }
 }
